a283510058e6ba78596755f86b36994bda0a4b2c
rishabh maurya <rishabh.mna.29@gmail.com>
2026-06-04 08:04:58 +0530

    day 8

---
+++ day 8/problem_30.c
@@ -0,0 +1,15 @@
+// wap to print number triangle
+#include<stdio.h>
+int main(){
+    int i;
+    for ( i = 0; i <=5; i++)
+    {
+        for ( int j = 1; j <=i; j++)
+        {
+            printf("%d",j);/* code */
+        }
+        printf("\n");
+        /* code */
+    }
+    return 0;
+}
