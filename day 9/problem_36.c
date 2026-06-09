1d1fd3f33b58354252c6aadbdcf7c02478586e3d
rishabh maurya <rishabh.mna.29@gmail.com>
2026-06-09 10:09:06 +0530

    day 9

---
+++ day 9/problem_36.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int i;
+    for ( i =1; i <=5; i++)
+    {
+        for ( int j =1; j <=5; j++)
+        {
+            if (i==1||i==5||j==1||j==5)
+            {
+                printf("*");/* code */
+            }
+            else
+            {
+                printf(" ");/* code */
+                /* code */
+            }
+            /* code */
+        }
+        printf("\n");
+        /* code */
+    }
+    return 0;
+}
